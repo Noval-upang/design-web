3530c6fb3a649a44f5f17ec5fd5b492bb07ab08b
Hy-Iam-Noval <upangwoy@gmail.com>
2023-12-13 22:26:43 +0700

    s

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,14 +3,13 @@ import About from "./About";
 import Contact from "./Contact";
 import Nav from "./Nav";
 import Proj from "./Proj";
-import getenv from 'getenv';
 
 function Home() {
    return (
       <>
          <Nav/>
          <div className="flex items-center justify-center h-[15vh]">
-            <span className="text-xl font-raleway font-medium">{getenv("TEST")}</span>
+            <span className="text-xl font-raleway font-medium">{process.env.TEST}</span>
          </div>
          <div className="fixed bottom-4 rigth-4"></div>
       </>
