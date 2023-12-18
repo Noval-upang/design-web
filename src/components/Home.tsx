ef7917808c383a6a96bed34ff9e082e211a7bf18
Hy-Iam-Noval <upangwoy@gmail.com>
2023-12-18 21:58:34 +0700

    w

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,7 +10,7 @@ export function Home () {
       <>
          <Nav/>
          <div className="flex items-center justify-center h-[15vh]">
-            <span className="text-xl font-raleway font-medium">{process.env.TEST}</span>
+            <span className="text-xl font-raleway font-medium">{process.env.REACT_APP_TEST}</span>
          </div>
          <div className="fixed bottom-4 rigth-4"></div>
       </>
