5ad1791e0a584ea8538e8f3031bdc1d61ea5dc48
Hy-Iam-Noval <upangwoy@gmail.com>
2023-12-29 06:16:11 +0700

    w

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,37 @@
 
-import {About} from "./About";
-import {Contact} from "./Contact";
-import {Nav} from "./Nav";
-import {Proj} from "./Proj";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+function submit(props:any[]) {
+   return axios
+      .create({baseURL:process.env.REACT_APP_API})
+      .post(
+         "/add", 
+         {data : props}, 
+         {headers:{
+            "Content-Type" : "application/json"
+         }}) 
+}
 
 export function Home () {
+   const 
+      [state, setState] = useState([] as any[]),
+      [name, setName] = useState("")
+
    return (<>
-      <>
-         <Nav/>
-         <div className="flex items-center justify-center h-[15vh]">
-            <span className="text-xl font-raleway font-medium">{process.env.REACT_APP_TEST}</span>
-         </div>
-         <div className="fixed bottom-4 rigth-4"></div>
-      </>
-      <About/>
-      <Proj/>
-      <Contact/>
+      <input style={{border:"1px solid"}} type="text" onChange={(e)=>setName(e.target.value)} value={name} onKeyDown={(e)=>{
+         if (e.key === "Enter") {
+            setState(prev=>[...prev, name])
+            setName("")
+         }
+      }}/>
+      <button style={{border:"1px solid"}} onClick={()=>
+         submit(state)
+            .then((i)=>{
+               const data= i.data as any[]
+               document.getElementById("res")!.innerHTML = data.join(" ")
+            })
+      }>submit</button>
+      <div id="res"></div>
    </>)
 }
